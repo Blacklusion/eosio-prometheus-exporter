d09632a2f929c9a3ac1b5f3d9b51bac293542672
blokcrafter-dan <dan@blokcrafters.com>
2022-11-24 15:24:17 -0600

    move the listen host from 127.0.0.1 to 0.0.0.0 to be reachable by docker

--- atomic-api/run.sh
+++ atomic-api/run.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"
 
-docker run -d --rm --name atomic-api -p 127.0.0.1:6102:8000 atomic-api
+docker run -d --rm --name atomic-api -p 0.0.0.0:6102:8000 atomic-api
--- blockchain-api/run.sh
+++ blockchain-api/run.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"
 
-docker run -d --rm --name blockchain-api -p 127.0.0.1:6100:8000 blockchain-api
+docker run -d --rm --name blockchain-api -p 0.0.0.0:6100:8000 blockchain-api
