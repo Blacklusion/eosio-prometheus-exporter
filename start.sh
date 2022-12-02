ee1522b413b80440063c5694e23ed65d7c87e2fc
blokcrafter-dan <dan@blokcrafters.com>
2022-12-02 05:38:15 -0600

    fix iptables stuff - delete it

--- start.sh
+++ start.sh
@@ -4,28 +4,28 @@ starting_dir="${PWD}"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"
 
-if [ -x ./set-ipt.sh ]; then
-  if ! ./set-ipt.sh; then
-    printf "There was a problem running the set-ipt.sh iptables security script.  Please investigate.\n"
-    exit 1
-  fi
-else
-  cat <<EOF
-
-You need to copy set-ipt.sh.example to set-ipt.sh and adjust the iptables settings in it
-to work in your environment.  Also make set-ipt.sh executable.
-The reason this is neccessary is that docker by default will allow full access through
-iptables for any published ports.  You almost certainly don't want this to happen.
-If you are protected by some other firewall mechanism (another device) other than
-iptables and/or ufw - then you might be able to ignore doing this.
-Currently the setup for the applications in this repository use ports 6100-6102.
-The example script blocks access to them from the outside world, with the exception
-of a management IP address.  After making set-ipt.sh please adjust the setting(s) in
-it to improve security for your system.
-
-EOF
-  exit 1
-fi
+#if [ -x ./set-ipt.sh ]; then
+#  if ! ./set-ipt.sh; then
+#    printf "There was a problem running the set-ipt.sh iptables security script.  Please investigate.\n"
+#    exit 1
+#  fi
+#else
+#  cat <<EOF
+#
+#You need to copy set-ipt.sh.example to set-ipt.sh and adjust the iptables settings in it
+#to work in your environment.  Also make set-ipt.sh executable.
+#The reason this is neccessary is that docker by default will allow full access through
+#iptables for any published ports.  You almost certainly don't want this to happen.
+#If you are protected by some other firewall mechanism (another device) other than
+#iptables and/or ufw - then you might be able to ignore doing this.
+#Currently the setup for the applications in this repository use ports 6100-6102.
+#The example script blocks access to them from the outside world, with the exception
+#of a management IP address.  After making set-ipt.sh please adjust the setting(s) in
+#it to improve security for your system.
+#
+#EOF
+#  exit 1
+#fi
 
 collectors="atomic-api blockchain-api hyperion-api"
 for collector in ${collectors}
